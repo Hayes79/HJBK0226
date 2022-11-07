a4ed7f0d1662ab6707917aa85ee27f33725577f8
Hayes79 <55575830+Hayes79@users.noreply.github.com>
2022-11-07 17:08:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const WEDDING_INVITATION_URL = "https://hjbk0226.netlify.app/";
-export const KAKAOTALK_API_TOKEN = "80152d9afb50311e1dfe62ad7466fe48";
+export const KAKAOTALK_API_TOKEN = "458d3dd4c2ac71819bcf466bcd73fc97";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
