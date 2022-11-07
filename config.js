b740c73b21a4b5d6ffe8ac245ed247013760005a
Hayes79 <55575830+Hayes79@users.noreply.github.com>
2022-11-07 12:58:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
 export const WEDDING_DATE = "2023년 02월 26일, 일요일 낮 12시 00분";
-export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
+export const WEDDING_LOCATION = "잠실롯데호텔월드, 3층 크리스탈볼룸";
 
 export const GROOM_NAME = "○○○";
 export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
