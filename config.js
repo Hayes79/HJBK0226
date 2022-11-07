52c50d8a6385745991cb234cd6b9cff4f5f7ab38
Hayes79 <55575830+Hayes79@users.noreply.github.com>
2022-11-07 17:03:51 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const WEDDING_INVITATION_URL = "http://localhost:8000/"";
-export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
+export const KAKAOTALK_API_TOKEN = "80152d9afb50311e1dfe62ad7466fe48";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
