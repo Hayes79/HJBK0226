08d3b9f560ffd8c4a67c59fcba33ebf44e237589
Hayes79 <55575830+Hayes79@users.noreply.github.com>
2022-11-16 10:23:41 +0900

    Update greeting.jsx

--- src/components/greeting.jsx
+++ src/components/greeting.jsx
@@ -59,19 +59,19 @@ const Greeting = () => {
       </Divider>
       <Image data-aos="fade-up" src={Flower} />
       <Content data-aos="fade-up">
-        서로 마주 보며 다져온 사랑을
+        그동안 키워온 사랑과 믿음의 마음으로
         <br />
         <br />
-        이제 함께 한곳을 바라보며 걸어갈 수 있는
+        연애를 마무리하고 부부의 연을 맺고자 하여
         <br />
         <br />
-        큰 사랑으로 키우고자 합니다.
+        설레이는 마음으로 그 첫걸음을 내딛고자 합니다.
         <br />
         <br />
-        저희 두 사람이 사랑의 이름으로 지켜나갈 수 있게
+        저희 두 사람이 두 손을 맞잡고 나아갈 새로운 길의 시작에
         <br />
         <br />
-        앞날을 축복해 주시면 감사하겠습니다.
+        참석해주셔서 응원하고 축하해주시면 감사하겠습니다.
       </Content>
       <GroomBride data-aos="fade-up">
         {GROOM_FATHER_NAME} · {GROOM_MOTHER_NAME}의 장남 {GROOM_NAME}
